4bc27daed2e3958bfa10a92bf1e089d4efceac60
foresight100 <1194798160@qq.com>
2020-09-12 18:48:38 +0800

    0912

--- test0911/src/com/wangqiuchen/Main.java
+++ test0911/src/com/wangqiuchen/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println(new Solution().combinationSum3(3,7));
+        System.out.println(new Solution().combinationSum3(3,12));
     }
 }
 
